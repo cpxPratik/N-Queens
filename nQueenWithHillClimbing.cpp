8bc572b22c871e4623c3e2426a11f5a3077ab9f0
cpxPratik <surajkrstha@gmail.com>
2015-08-28 23:46:15 +0545

    Random-restart hill climbing applied. Also code is optimized to make little faster.

--- nQueenWithHillClimbing.cpp
+++ nQueenWithHillClimbing.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <ctime>
 using namespace std;
-const int N = 4;
-inline int findConflicts(int queenStates[], int addValues[], int subtractValues[]) {
-	int conflicts = 0;
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
+const int N = 20;
+inline void findConflicts(int queenStates[], int addValues[], int subtractValues[], int& conflicts) {
+	//int conflicts = 0;
+	conflicts = 0;
+	static int i;
+	static int j;
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < N; j++) {
 			if (i == j) continue;
 			if (queenStates[i] == queenStates[j])
 				conflicts++;
@@ -15,72 +18,89 @@ inline int findConflicts(int queenStates[], int addValues[], int subtractValues[
 				conflicts++;
 		}
 	}
-	return conflicts;
+	//return conflicts;
 }
 
 
 int main() {
-	
+
 	srand(time(NULL));
 	int currentQueenStates[N], tempQueens[N];
 	int currentConflicts, tempConflicts;
 	int addValues[N];
 	int subtractValues[N];
 	int col, row;
-	
-	for (col = 0; col < N; col++) {
-		currentQueenStates[col] = rand() % N;
-		addValues[col] = currentQueenStates[col] + col;
-		subtractValues[col] = currentQueenStates[col] - col;
-	}
-	
-	currentConflicts = findConflicts(currentQueenStates, addValues, subtractValues);
+	bool generateRandomState = true; // do we have to restart climbing with another randomly generated initial QueenPositions
 	unsigned long count = 0;
-	while (currentConflicts > 0) {
-		bool newState = false;
+	int i;
+	while (generateRandomState) {
+		count++;
+		cout << count << "\t";
 		for (col = 0; col < N; col++) {
-			for (row = 0; row < N; row++) {
-				for (int i = 0; i < N; i++) {
-					tempQueens[i] = currentQueenStates[i];
-					addValues[i] = tempQueens[i] + i;
-					subtractValues[i] = tempQueens[i] - i;
-				}
-				if (currentQueenStates[col] != row) {
-					tempQueens[col] = row;
-					addValues[col] = tempQueens[col] + col;
-					subtractValues[col] = tempQueens[col] - col;
-				}
-				tempConflicts = findConflicts(tempQueens, addValues, subtractValues);
-				if (tempConflicts < currentConflicts) {
-					for (int i = 0; i < N; i++) {
-						currentQueenStates[i] = tempQueens[i];
+			currentQueenStates[col] = rand() % N;
+			addValues[col] = currentQueenStates[col] + col;
+			subtractValues[col] = currentQueenStates[col] - col;
+		}
+
+		findConflicts(currentQueenStates, addValues, subtractValues, currentConflicts);
+		for (i = 0; i < N; i++) {
+			tempQueens[i] = currentQueenStates[i];
+			addValues[i] = tempQueens[i] + i;
+			subtractValues[i] = tempQueens[i] - i;
+		}
+		while (currentConflicts > 0) {
+			bool newState = false;
+			for (col = 0; col < N; col++) {
+				for (row = 0; row < N; row++) {
+
+					if (currentQueenStates[col] != row) {
+						tempQueens[col] = row;
+						addValues[col] = tempQueens[col] + col;
+						subtractValues[col] = tempQueens[col] - col;
+						findConflicts(tempQueens, addValues, subtractValues, tempConflicts);
+						if (tempConflicts < currentConflicts) {
+							//	for (i = 0; i < N; i++) {
+							//	currentQueenStates[i] = tempQueens[i];
+							//}
+							currentQueenStates[col] = row;
+							newState = true;
+							break;
+						}
+						else {
+							tempQueens[col] = currentQueenStates[col];
+							addValues[col] = tempQueens[col] + col;
+							subtractValues[col] = tempQueens[col] - col;
+						}
 					}
-					newState = true;
-					break;
 				}
+				if (newState)
+					break;
 			}
-			if (newState)
+			if (!newState) {
+				//count++;
+				//cout << count << "\t";
+				//cout << "Stuck in Local minimum(for cost). Every move of a single queen increases conflicts.\n"
+				//	<< "State space with local minimum is given below:" << endl;
 				break;
+			}
+			findConflicts(currentQueenStates, addValues, subtractValues, currentConflicts);
 		}
-		if (!newState) {
-			//count++;
-			cout << "Stuck in Local minimum(for cost). Every move of a single queen increases conflicts.\n"
-				<<"State space with local minimum is given below:"<<endl;
-			break;
-		}
-		currentConflicts = findConflicts(currentQueenStates, addValues, subtractValues);
+		if (currentConflicts == 0)
+			generateRandomState = false;
+
 	}
+	
 	//char *board[N] = { "----",  "----", "----", "----" };
 	int queenPositions[N];
 	for (col = 0; col < N; col++) {
 		queenPositions[currentQueenStates[col]] = col;
 	}
-
+	cout << endl;
 	for (row = 0; row < N; row++) {
 		for (col = 0; col < queenPositions[row]; col++) {
 			cout << "-";
 		}
-		cout << "Q";
+		cout << "+";
 		for (col = queenPositions[row]+1; col < N; col++) {
 			cout << "-";
 		}
